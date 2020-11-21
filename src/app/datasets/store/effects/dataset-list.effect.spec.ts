1ed7e1fc7440c921ae40671e14960b8d086b3a08
Michael Czolko <michael@czolko.cz>
2020-11-21 20:36:02 +0100

    dataset-list effect test

---
+++ src/app/datasets/store/effects/dataset-list.effect.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed } from '@angular/core/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { provideMockActions } from '@ngrx/effects/testing'
+import { provideMockStore } from '@ngrx/store/testing'
+import { Observable, of } from 'rxjs'
+
+import { DatasetListEffect } from './dataset-list.effect'
+import * as datasetListActions from '../actions/dataset-list.action'
+
+describe('DatasetListEffect', () => {
+  let actions$: Observable<any>
+  let effects: DatasetListEffect
+  let httpTestingController: HttpTestingController
+
+  beforeEach(() => {
+    actions$ = of(datasetListActions.LOAD_DATASET_LIST)
+
+    TestBed.configureTestingModule({
+      providers: [
+        DatasetListEffect,
+        provideMockStore(),
+        provideMockActions(() => actions$)
+      ],
+      imports: [ HttpClientTestingModule ]
+    })
+
+    httpTestingController = TestBed.inject(HttpTestingController)
+    effects = TestBed.inject(DatasetListEffect)
+  })
+
+  afterEach(() => {
+    httpTestingController.verify()
+  })
+
+  it('should be created', () => {
+    expect(effects).toBeTruthy()
+  })
+
+  it(`should dispatch "${datasetListActions.LOAD_DATASET_LIST_SUCCESS}" when OK`, () => {
+    const data = [ 'dataset-one', 'dataset-two' ]
+
+    actions$ = of({ type: datasetListActions.LOAD_DATASET_LIST })
+    effects.loadDatasetList$.subscribe(action => {
+      expect(action.type).toEqual(datasetListActions.LOAD_DATASET_LIST_SUCCESS)
+      expect(action.payload).toEqual(data)
+    })
+
+    const req = httpTestingController.expectOne(
+      'http://localhost:8080/dataset'
+    )
+    req.flush(data)
+  })
+
+})
