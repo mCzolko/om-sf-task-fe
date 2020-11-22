aea6a89c4a530cdde573f81916ebdaee2c6b1067
Michael Czolko <michael@czolko.cz>
2020-11-22 19:58:44 +0100

    Create dataset-data.reducer.spec.ts

---
+++ src/app/datasets/store/reducers/dataset-data.reducer.spec.ts
@@ -0,0 +1,33 @@
+import * as fromActions from '../actions/dataset-data.action'
+import * as fromReducer from './dataset-data.reducer'
+import { DatasetDataUpdate } from '../../models/dataset-data-update.model'
+
+describe('Store > Dataset > Data Reducer', () => {
+
+  it('should transform data into state', () => {
+    const { initialState } = fromReducer
+    const updates: fromReducer.DataRow[] = [
+      {
+        id: 1,
+        name: "John",
+      }, {
+        id: 2,
+        name: "Joe"
+      }
+    ]
+    const payload: DatasetDataUpdate = {
+      action: 'data_update',
+      topic: 'testtopic',
+      payload: updates
+    }
+    const action = new fromActions.UpdateData(payload)
+    const state = fromReducer.reducer(initialState, action)
+
+    expect(state).toEqual({
+      testtopic: {
+        1: { id: 1, name: 'John' },
+        2: { id: 2, name: 'Joe'}
+      }
+    })
+  })
+})
